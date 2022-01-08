c292317e0b2167f5abb7e7e23bd81a5348f6204e
vuralkurt <vuralkurt3535@gmail.com>
2022-01-08 15:11:45 +0100

    derr

--- src/OCA_SORULAR/Q23.java
+++ src/OCA_SORULAR/Q23.java
@@ -1,5 +1,7 @@
 package OCA_SORULAR;
 
+import java.util.Arrays;
+
 public class Q23 {
     public static void main(String[] args) {
          /*
@@ -10,5 +12,29 @@ public class Q23 {
         input : {3, 0, 4, 2, 0}
         output: {3, 4, 2, 0, 0}
          */
+
+        Integer aar[]={33,0,43,29,0,78};
+
+        Integer aar1[]=new Integer[aar.length];
+
+        int count = 0;
+        int sayac=1;
+
+        for (int i = 0; i <aar.length ; i++) {
+
+            if(aar[i]!=0){
+                aar1[count]=aar[i];
+                count++;
+            }
+
+            if(aar[i]==0){
+                aar1[aar.length-sayac]=aar[i];
+                sayac++;
+            }
+            
+        }
+        System.out.println(Arrays.toString(aar));
+        System.out.println(Arrays.toString(aar1));
+
     }
 }
